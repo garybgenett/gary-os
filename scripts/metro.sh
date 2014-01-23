8529202afae27aab6a78e4d4299ac88cfac2af56
Gary B. Genett <me@garybgenett.net>
2014-01-22 16:50:33 -0800

    Improved variables/formatting in "metro.sh" script.

--- scripts/metro.sh
+++ scripts/metro.sh
@@ -7,21 +7,40 @@ declare ISO="/.g/_data/_target/iso"
 
 ########################################
 
-declare SRC="${BLD}/funtoo/metro"
-declare DST="${BLD}/_metro"
-
-declare PORT="${BLD}/funtoo/portage"
-declare DIST="${DST}/.distfiles"
-declare TEMP="${DST}/.temp"
+declare SMET="${BLD}/funtoo/metro"
+declare SPRT="${BLD}/funtoo/portage"
 
 declare TYPE="funtoo-stable"
 declare PLAT="x86-64bit"
 declare ARCH="core2_64"
-declare DOUT="${DST}/${TYPE}/${PLAT}/${ARCH}"
+
+declare DEST="${BLD}/_metro"
+declare DFIL="${DEST}/.distfiles"
+declare DTMP="${DEST}/.temp"
+declare DOUT="${DEST}/${TYPE}/${PLAT}/${ARCH}"
 
 ########################################
 
-declare NAME="$(cat ${SRC}/etc/builds/${TYPE}/build.conf	2>/dev/null |
+declare SAFE_ENV="prompt -z"
+export CCACHE_DIR=
+
+declare METRO_CMD="${SMET}/metro \
+	--libdir ${SMET} \
+	--verbose \
+	--debug \
+	multi:		yes \
+	multi/mode:	full \
+	path/mirror:	${DEST} \
+	path/distfiles:	${DFIL} \
+	path/tmp:	${DTMP} \
+	target/build:	${TYPE} \
+	target/subarch:	${ARCH} \
+	target/version:	$(date --iso=d) \
+"
+
+########################################
+
+declare NAME="$(cat ${SMET}/etc/builds/${TYPE}/build.conf	2>/dev/null |
 	${SED} -n "s/^name[:][ ]//gp")"
 declare DATE="$(ls {${ISO},${DOUT}/*}/stage3-*${TYPE}*.tar.xz	2>/dev/null |
 	${SED} "s/^.+([0-9]{4}-[0-9]{2}-[0-9]{2}).+$/\1/g" |
@@ -30,30 +49,32 @@ declare DATE="$(ls {${ISO},${DOUT}/*}/stage3-*${TYPE}*.tar.xz	2>/dev/null |
 
 { [[ -z ${NAME} ]] || [[ -z ${DATE} ]]; } && exit 1
 
-########################################
-
-declare SAFE_ENV="prompt -z"
-export CCACHE_DIR=
-
-declare METRO_CMD="${SRC}/metro \
-	--libdir ${SRC} \
-	--verbose \
-	--debug"
-
 ################################################################################
 
-${MKDIR} ${DST}
+${MKDIR} ${DEST}
+
+########################################
+
 #>>>${RM} /usr/bin/metro
-#>>>${LN} ${SRC}/metro /usr/bin/metro
+#>>>${LN} ${SMET}/metro /usr/bin/metro
 #>>>${RM} /usr/lib/metro
-#>>>${LN} ${SRC} /usr/lib/metro
+#>>>${LN} ${SMET} /usr/lib/metro
 #>>>${RM} /var/tmp/metro
-#>>>${LN} ${TEMP} /var/tmp/metro
+#>>>${LN} ${DTMP} /var/tmp/metro
 
 ${MKDIR} ${DOUT}/.control/{strategy,version}
 echo -en "local\n"	>${DOUT}/.control/strategy/build
 echo -en "stage3\n"	>${DOUT}/.control/strategy/seed
 echo -en "${DATE}\n"	>${DOUT}/.control/version/stage3
+
+if [[ ! -d ${DTMP}/cache/cloned-repositories/${NAME} ]]; then
+	${MKDIR}		${DTMP}/cache/cloned-repositories/${NAME}
+	${RSYNC_U} ${SPRT}.git/	${DTMP}/cache/cloned-repositories/${NAME}.git
+	${RM}			${DTMP}/cache/cloned-repositories/${NAME}/.git
+	${LN} ../${NAME}.git	${DTMP}/cache/cloned-repositories/${NAME}/.git
+fi
+(cd ${DTMP}/cache/cloned-repositories/${NAME} &&
+	${GIT} checkout --force funtoo.org)
 
 declare FILE=
 for FILE in $(ls ${ISO}/stage3-*${TYPE}*.tar.xz |
@@ -61,17 +82,8 @@ for FILE in $(ls ${ISO}/stage3-*${TYPE}*.tar.xz |
 	sort -n)
 do
 	${MKDIR} ${DOUT}/${FILE}
-	${RSYNC_C} ${ISO}/stage3-*${TYPE}*-${FILE}.tar.xz ${DOUT}/${FILE}/
+	${RSYNC_U} ${ISO}/stage3-*${TYPE}*-${FILE}.tar.xz ${DOUT}/${FILE}/
 done
-
-if [[ ! -d ${TEMP}/cache/cloned-repositories/${NAME} ]]; then
-	${MKDIR}		${TEMP}/cache/cloned-repositories/${NAME}
-	${RSYNC_U} ${PORT}.git/	${TEMP}/cache/cloned-repositories/${NAME}.git
-	${RM}			${TEMP}/cache/cloned-repositories/${NAME}/.git
-	${LN} ../${NAME}.git	${TEMP}/cache/cloned-repositories/${NAME}/.git
-fi
-(cd ${TEMP}/cache/cloned-repositories/${NAME} &&
-	${GIT} checkout --force funtoo.org)
 
 ########################################
 
@@ -82,16 +94,10 @@ echo -en "DATE: ${DATE}\n"
 echo -en "\n"
 ${SAFE_ENV} env
 
+########################################
+
 echo -en "\n"
-${SAFE_ENV} ${METRO_CMD} \
-	multi: yes \
-	multi/mode: full \
-	path/mirror: ${DST} \
-	path/distfiles: ${DIST} \
-	path/tmp: ${TEMP} \
-	target/build: ${TYPE} \
-	target/subarch: ${ARCH} \
-	target/version: $(date --iso=d) || exit 1
+${SAFE_ENV} ${METRO_CMD} || exit 1
 
 exit 0
 ################################################################################
