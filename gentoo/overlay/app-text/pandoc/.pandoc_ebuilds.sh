62dedcd7d95b4796a5ab3e199f7d065f8f61955b
Gary B. Genett <me@garybgenett.net>
2021-08-07 11:57:04 -0700

    Special treatment for "pandoc" in "gentoo".
    	* Brand new version!

--- gentoo/overlay/app-text/pandoc/.pandoc_ebuilds.sh
+++ gentoo/overlay/app-text/pandoc/.pandoc_ebuilds.sh
@@ -2,28 +2,38 @@
 source ${HOME}/.bashrc
 set -e
 ################################################################################
+# ./.pandoc_ebuilds.sh =
+# ./.pandoc_ebuilds.sh /
 # ./.pandoc_ebuilds.sh -l
-# ./.pandoc_ebuilds.sh [ - | + | / ]
+########################################
+# ./.pandoc_ebuilds.sh [ - | + ]
 # ./.pandoc_ebuilds.sh texlive [ = | module ]
 # ./.pandoc_ebuilds.sh dev-texlive/texlive-basic	[ "" | - | version ]
 # ./.pandoc_ebuilds.sh dev-lang/ghc			[ "" | - | version ]
-# ./.pandoc_ebuilds.sh app-text/pandoc =
+########################################
+# FILE="yaml"; (cd ./gentoo/overlay/app-text/pandoc; ./.pandoc_ebuilds.sh dev-haskell/${FILE} - && ./.pandoc_ebuilds.sh app-text/pandoc /)
+# FILE="basic"; (cd ./gentoo/overlay/app-text/pandoc; ./.pandoc_ebuilds.sh dev-texlive/texlive-${FILE} - && ./.pandoc_ebuilds.sh app-text/pandoc /)
+########################################
+# git-commit -- $(./gentoo/overlay/app-text/pandoc/.pandoc_ebuilds.sh ^)
 ################################################################################
 
 export TOOR="-x"
+
+export PANPKG="app-text/pandoc"
+export TEXPKG="app-text/texlive"
 export TANURI="http://mirror.ctan.org/systems/texlive/tlnet/archive"
 
 ################################################################################
 
-export PANDIR="$(dirname $(realpath ${0}))"
+export PANDIR="$(dirname $(realpath ${_SELF}))"
 export SETDIR="${PANDIR/%\/gentoo\/overlay\/app-text\/pandoc}"
 source ${SETDIR}/gentoo/_system
 
 ########################################
 
-export COMMIT="$(${SED} -n "s|^[^#].+id=||gp"				${PANDIR}/.rationale | tail -n1)"
-export PANDOC="$(${SED} -n "s|^[=]app-text/pandoc[-](.+)$|\1|gp"	${PANDIR}/.rationale | tail -n1)"
-export TXLIVE="$(${SED} -n "s|^[=]app-text/texlive[-](.+)$|\1|gp"	${PANDIR}/.rationale | tail -n1)"
+export COMMIT="$(${SED} -n "s|^[^#].+id=||gp"			${PANDIR}/.rationale | tail -n1)"
+export PANDOC="$(${SED} -n "s|^[=]${PANPKG}[-](.+)$|\1|gp"	${PANDIR}/.rationale | tail -n1)"
+export TXLIVE="$(${SED} -n "s|^[=]${TEXPKG}[-](.+)$|\1|gp"	${PANDIR}/.rationale | tail -n1)"
 export TLYEAR="${TXLIVE/%-r[0-9]*}"
 
 ########################################
@@ -36,16 +46,33 @@ if [[ ${1} == -l ]]; then
 	for FILE in $(${SED} -n "s|^[-+][ ]||gp" ${PANDIR}/.rationale); do
 		echo -en "\n[${FILE}]\n"
 		cat ${SETDIR}/gentoo/overlay/${FILE}/.rationale
-		${GREP} "#>>>." ${SETDIR}/gentoo/overlay/${FILE}/*.ebuild || true
+		${GREP} "^#>>>" ${SETDIR}/gentoo/overlay/${FILE}/*.ebuild || true
 	done
 	exit 0
 fi
 
 ########################################
 
-if [[ ${1} == [-+/] ]]; then
-	for FILE in $(${SED} -n "s|^[${1}][ ]||gp" ./.rationale); do
-		${0} ${FILE} -
+if [[ ${1} == [-+] ]]; then
+	for FILE in $(${SED} -n "s|^[${1}][ ]||gp" ${PANDIR}/.rationale); do
+		${_SELF} ${FILE} -
+	done
+	exit 0
+fi
+
+########################################
+
+if [[ ${1} == "^" ]]; then
+	${SED} -n "s|^[=]([^/]+)[/](.+)[-]([0-9].+)$|\1/\2|gp" ${PANDIR}/.rationale |
+		while read -r FILE; do
+			echo "$(realpath --relative-to="${PWD}" ${SETDIR}/gentoo/overlay/${FILE})"
+		done
+	for FILE in \
+		dev-haskell \
+		dev-tex \
+		dev-texlive \
+	; do
+		echo "$(realpath --relative-to="${PWD}" ${SETDIR}/gentoo/overlay/${FILE})"
 	done
 	exit 0
 fi
@@ -54,6 +81,15 @@ fi
 
 if [[ -n ${1} ]]; then declare PKG="${1}" && shift && declare NAM="$(basename ${PKG})"; else exit 1; fi
 if [[ -n ${1} ]]; then declare VER="${1}" && shift; fi
+
+if [[ ${PKG} == "=" ]]; then
+	PKG="${PANPKG}"
+	VER="${PKG}"
+fi
+if [[ ${PKG} == "/" ]]; then
+	PKG="${PANPKG}"
+	VER="/"
+fi
 
 ########################################
 
@@ -76,11 +112,13 @@ if [[ ${PKG} == "texlive" ]]; then
 			fi |
 				${SED} "s/[.](source|doc)$//g" | sort -u
 		); do
-			${0} ${PKG} ${FILE} || true
+			${_SELF} ${PKG} ${FILE} || true
 		done
 	fi
 	exit 0
 fi
+
+########################################
 
 if [[ ${VER} == "-" ]]; then VER="$(
 	${WGET_C} -O - "https://gitweb.gentoo.org/repo/gentoo.git/tree/${PKG}?id=${COMMIT}" 2>/dev/null |
@@ -99,17 +137,24 @@ fi
 
 if [[ ! -f			${SETDIR}/gentoo/overlay/${PKG}/.rationale ]]; then
 	${MKDIR}		${SETDIR}/gentoo/overlay/${PKG}
-	echo "pandoc version"	>${SETDIR}/gentoo/overlay/${PKG}/.rationale
+	echo "# pandoc version"	>${SETDIR}/gentoo/overlay/${PKG}/.rationale
 fi
 
-if [[ ${VER} != "=" ]]; then
-	${SETDIR}/gentoo/_system ${TOOR} -o ${NAM} ${NAM}-${VER}.ebuild ${COMMIT}
-fi
-if [[ ${PKG} == app-text/pandoc ]]; then
-	${SETDIR}/gentoo/_system ${TOOR} -s -e $(${GREP} "^[=]" ./.rationale) \
-		=app-text/texlive-${TXLIVE} \
-		=app-text/pandoc-${PANDOC}
+if [[ ${PKG} == ${PANPKG} ]]; then
+	if [[ ${VER} == ${PKG} ]]; then
+		${SED} -n "s|^[=](.+)[-]([0-9].+)$|\1 \2|gp" ${PANDIR}/.rationale |
+			${GREP} -v "${PANPKG}" |
+			while read -r FILE; do
+				${_SELF} ${FILE}
+			done
+	fi
+	${SETDIR}/gentoo/_system -q ${TOOR} -s -e $(${GREP} "^[=]" ${PANDIR}/.rationale) \
+		=${TEXPKG}-${TXLIVE} \
+		=${PANPKG}-${PANDOC} \
+		|| exit 1
 else
+	${SETDIR}/gentoo/_system -q ${TOOR} -o ${NAM} ${NAM}-${VER}.ebuild ${COMMIT} \
+		|| exit 1
 	if [[ -f ${SETDIR}/gentoo/overlay/${PKG}/.source/.keep ]]; then
 		echo "- ${PKG}"	>>${PANDIR}/.rationale
 	else
